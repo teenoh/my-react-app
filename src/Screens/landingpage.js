235ac02ec07b86c10d7992a56c001bec310c73df
chisouche <jenniferuc.ju@gmail.com>
2023-08-02 15:32:41 +0100

    landingpage update

--- src/Screens/landingpage.js
+++ src/Screens/landingpage.js
@@ -9,14 +9,14 @@ const LandingPage = () => {
     <div className='background-image'>
       <Header />
       <main>
-      <h1 className='order'>Order Food to your door!</h1>
+      <p className='order'>Order food to your door</p>
         <section className="hero">
 
           <form>
               <div className="container">
                 <div className="column address">
                 <i className='fas fa-map-marker-alt location' ></i>
-                  <p id="address-search" placeholder="Enter an address...">
+                  <p id="address-search" placeholder="Enter delivery address...">
                   </p>
 
                 </div>
@@ -24,30 +24,23 @@ const LandingPage = () => {
                 <div className="column time">
                   <p>
                   <select id="fruit-selection" name="Pick Up now">
-                    <option value="apple">Apple</option>
-                    <option value="banana">Banana</option>
-                    <option value="orange">Orange</option>
-                    <option value="strawberry">Strawberry</option>
+                    <option value="deliver now" > Deliver now</option>
+                    <option value="schedule"  >Schedule for later</option>
                   </select>
                   </p>
-                 
                 </div>
 
-                <div className ="column">
-                  <button type='button'>
+                <div className ="column find-food">
+                  <button type='button' className='find-foodbtn' >
                     Find Food
                   </button>
                 </div>
 
               </div>
           </form>
-
-  
-
-          {/* <label htmlFor="address-search">Search Address:</label>
-              <input type="text" id="address-search" placeholder="Enter an address..." />
-              <input type="submit" value="Search" /> */}
-
+          <p className='signin_link'>
+          <span>Sign in </span>  <span>for your recent addresses</span> 
+          </p>
         </section>
 
         {/* Add more sections as needed */}
